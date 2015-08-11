775b644487c3c1d979bb5c8fbb1aea01f12251c8
techjunkie007 <connectankityadav@gmail.com>
2015-08-11 05:43:09 +0530

    Modified next entry as Blade

--- app/views/next_entry.blade.php
+++ app/views/next_entry.blade.php
@@ -1,140 +1,126 @@
-<html>
-<head>
-    <title>Late Entry | Next Entry</title>
-    
-    <!-- bootstrapcdn -->
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <!-- css file -->
-    <link rel="stylesheet" type="text/css" href="../public/css/inside.css">
-    <!-- css file -->
-    <link rel="stylesheet" type="text/css" href="../public/css/mainpage.css">     
-    <!-- clock1 css-->
-    <link rel="stylesheet" type="text/css" href="../public/css/clock1.css">
-    <!-- register css -->
-    <link rel="stylesheet" type="text/css" href="../public/css/register.css">
+<!-- Extends Master -->
+@extends('master')
 
-</head>
+<!-- Yield Page Header Title-->
+@section('title', 'Late Entry | Today\'s Entry')
 
-<body>
+<!-- Yield Logout Button -->
+@section('logout_button')
 
-    <div class="container-fluid">
-        <!-- header -->
-        <div class="header row">
-            <div class="col-md-2 col-sm-1"></div>
-            <h1 class="text-center center col-md-7 col-sm-8">AKGEC LATE ENTRY SYSTEM</h1>
-            <!-- empty -->
-            {!!  Form::open(['route' => 'logout_submit']) !!}
-            <div class="col-md-1 col-sm-1"></div>
-            <!-- logout button at top -->
-            
+    <!-- Logout Form Open -->
+    {{ Form::open(['route' => 'logout_submit']) }}
+    <!-- Logout Button -->
+    <button class="btn btn-default col-md-1 col-sm-1 logout">
+    <span class="glyphicon glyphicon-off" aria-hidden="true">
+    </span>
+    <span> Logout</span>
+    <!-- Logout Form Close -->
+    {{ Form::close() }}
 
-            <!-- {!!Form::submit('Logout', array('class' => 'btn btn-default col-md-1 col-sm-1 logout')  ) !!} -->
+@endsection
 
-            
-            <button class="btn btn-default col-md-1 col-sm-1 logout">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-                <span> Logout</span>
-                <!-- empty -->
-                {!! Form::close() !!}
-                <!-- empty -->
-                <div class="col-md-1 col-sm-1"></div>               
+<!-- Yield Mid-Content -->
+@section('mid_content')
+
+    <div class="container block1">
+        <div class="row InformationBox">
+            <div class="col-md-3 col-sm-4 thumbnail">
+            <!-- Thumbnail of Student -->
+            <?php 
+            $image_path="images/".$info->student_id.".jpg";
+            echo "<img src=".$image_path.">";
+            ?>
             </div>
-            {!! Form::open(['route' => 'register_entry']) !!}
-
-            <div class="container block1">
-                <div class="row InformationBox">
-                    <div class="col-md-3 col-sm-3 thumbnail">
-                     <?php 
-                     $image_path="images/".$info->student_id.".jpg";
-                     echo "<img src=".$image_path.">";
-                     ?>
-                 </div>
-                 <div class="col-md-1 col-sm-1 border"></div>
-                 <div class="col-md-8 col-sm-6 Info">
-                    <ul>
-                        <li class=" row Name">{{ $info->student_name  }}</li><hr>
-                        <li class="row St_no">{{ $info->student_id }}</li>
-                        <li class="row Branch"><?php
-                        switch($info->branch){
-                            case 'CS': {
+            <!-- Empty -->
+            <div class="col-md-1 col-sm-1">
+            </div>
+            <div class="col-md-8 col-sm-8 Info">
+                <ul>
+                    <!-- Student's Name -->
+                    <li class=" row Name">{{ $info->student_name  }}</li><hr>
+                    <!-- Student Number -->
+                    <li class="row St_no">{{ $info->student_id }}</li>
+                    <!-- Student's Branch -->
+                    <li class="row Branch">
+                        <?php
+                        switch($info->branch)
+                        {
+                            case 'CS': 
+                            {
                                 $branch="Computer Science";
                                 break;
                             }
-                            case 'IT': {
+                            case 'IT': 
+                            {
                                 $branch="Information Technology";
                                 break;
                             } 
-                            case 'EN': {
+                            case 'EN': 
+                            {
                                 $branch="Electrical Engineering";
                                 break;
                             }
-                            case 'EC': {
+                            case 'EC': 
+                            {
                                 $branch="Electronics & Communication";
                                 break;
                             }
-                            case 'ME': {
+                            case 'ME': 
+                            {
                                 $branch="Mechanical";
                                 break;
                             }
-                            case 'EI': {
+                            case 'EI': 
+                            {
                                 $branch="Electronics & Instrumentation";
                                 break;
                             } 
-                            case 'CE': {
+                            case 'CE': 
+                            {
                                 $branch="Civil Engineering";
                                 break;
                             }
-                            case 'MCA': {
+                            case 'MCA': 
+                            {
                                 $branch="MCA";
                                 break;
                             }
-                            case 'MBA': {
+                            case 'MBA': 
+                            {
                                 $branch="MBA";
                                 break;
                             }
                         }
                         echo $branch;
-                        ?></li>
-
-                        <li class="row Text">
-                            <?php
-                            $count=0;
-                            ?>
-                            @foreach ($entries as $entry) 
-                            $count++;
-                            @endforeach
-                            {{$info->student_name." has ".$count." Late Entries till now."}}
-                            </li>
-                        </ul>
-                    </div>
-
-                   <!--  <div class="row"><button class="btn-lg btn-warning register_button col-md-4 col-md-offset-8 col-sm-4">Register Late Entry</button></div> -->
-                    
-                    <div class="row">
-                        {!!Form::submit('Register Late Entry', array('class' => 'btn-lg btn-warning register_button col-md-4 col-md-offset-8 col-sm-4', 'id' => 'loginButton')  ) !!}
-                    </div>
-
+                        ?>
+                    </li>
+                    <li class="row Text">
+                        <!-- Number of Entries Message -->
+                        <?php
+                        $count=0;
+                        foreach ($entries as $entry) 
+                        $count++;
+                        ?>         
+                        {{$info->student_name." has ".$count." Late Entries till now."}} 
+                    </li>
+                </ul>
+                <!-- Register Entry Form Open -->
+                {{ Form::open(['route' => 'register_entry']) }}
+                <div class="row">
+                    <!-- Submit Entry Button -->
+                    {{ Form::submit('Register Late Entry', array('class' => 'btn-lg btn-warning register_button', 'id' => 'loginButton')  ) }}
                 </div>
-
+                <!-- Register Entry Form Close -->
+                {{ Form::close() }}
             </div>
-              {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
 
 
-            <!-- footer -->
-            <div class="footer row">
-                <h4 class="text-center">&copy;<strong>SOFTWARE INCUBATOR</strong></h4>
-            </div>
-        </div>                              
 
 
-        <!-- javascript -->
 
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
-        <!-- jquery -->
-        <script type="text/javascript" src="jquery.js"></script>
-        <!-- bootstrap -->  
-        <script type="text/javascript" src="bootstrap.js"></script>     
-    </body>
-    </html>
+
+
